9b95dc889f79728e834d34886864411de31bc74f
zipmic <mail@code.dk>
2026-02-20 23:55:34 +0100

    Code scene updated with gun

--- Assets/Scripts/SniperGun.cs
+++ Assets/Scripts/SniperGun.cs
@@ -1,16 +1,80 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class SniperGun : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Shooting")]
+    [SerializeField] private Camera shootCamera;
+    [SerializeField] private float shootDistance = 100f;
+    [SerializeField] private string enemyTag = "Enemy";
+
+    [Header("Zoom")]
+    [SerializeField] private CinemachineVirtualCamera zoomCamera;
+    [SerializeField] private float zoomFov = 20f;
+    [SerializeField] private GameObject zoomOverlay;
+
+    private float _defaultFov;
+
     void Start()
     {
-        
+        if (zoomCamera != null)
+        {
+            _defaultFov = zoomCamera.m_Lens.FieldOfView;
+        }
+
+        if (zoomOverlay != null)
+        {
+            zoomOverlay.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleShoot();
+        HandleZoom();
+    }
+
+    private void HandleShoot()
+    {
+        if (shootCamera == null)
+        {
+            return;
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = shootCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            if (Physics.Raycast(ray, out RaycastHit hit, shootDistance))
+            {
+                if (hit.collider.CompareTag(enemyTag))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void HandleZoom()
+    {
+        if (zoomCamera == null)
+        {
+            return;
+        }
+
+        bool isZooming = Mouse.current != null && Mouse.current.rightButton.isPressed;
+        if (isZooming)
+        {
+            zoomCamera.m_Lens.FieldOfView = zoomFov;
+        }
+        else
+        {
+            zoomCamera.m_Lens.FieldOfView = _defaultFov;
+        }
+
+        if (zoomOverlay != null)
+        {
+            zoomOverlay.SetActive(isZooming);
+        }
     }
 }
